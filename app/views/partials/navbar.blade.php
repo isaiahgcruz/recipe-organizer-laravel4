19f193ef68fd71f45ea5aa7ac272b744c940d28d
Isaiah Cruz <isaiahgcruz@gmail.com>
2016-07-01 17:00:21 +0800

    Test

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -10,7 +10,7 @@
                </button>
                <!-- Branding Image -->
                <a class="navbar-brand" href="{{ url('/') }}">
-                   Laravel
+                   RecipeOrganizer
                </a>
            </div>
            <div class="collapse navbar-collapse" id="app-navbar-collapse">
